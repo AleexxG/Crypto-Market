e34b025657516367d152f9475891b00ef87760bf
AleexxG <alexanderg.sln@gmail.com>
2022-12-31 15:46:46 +0100

    Top Gainers and Losers

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -2,68 +2,20 @@ import React from "react";
 import './Info.css';
 
 export default function Info(props) {
-    const [coinsInfo, setCoinsInfo] = React.useState([]);
-
-    React.useEffect(() => {
-        (async () => {
-            const endpoint = new URL("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=5&page=1&sparkline=false&price_change_percentage=24h")
-
-            const response = await fetch(endpoint);
-            const data = await response.json();
-
-            setCoinsInfo(data);
-        })();
-    }, [])
-
-    const displayCoinsInfo = coinsInfo.map(coin => {
-        return (
-            <tr className="coin_info_cont" key={coin.id}>
-                <th className="coin_text" scope="row">
-                    <img alt="Coin Logo" src={coin.image}></img>
-                    <div className="crypto_text">
-                        <b className="coin_name">{coin.name}</b>
-                        <p className="coin_name_short">{coin.symbol}</p>
-                    </div>
-                </th>
-                <td className="coin_info_num">
-                    <p className="coin_price">${coin.current_price}</p>
-                    <p className="coin_change">{coin.price_change_percentage_24h}</p>
-                </td>
-            </tr>
-        )
-    })
 
     return(
-        <div className="info">
-            <aside className="gainers">
-                <h3>Biggest Gainers <span>24h</span> </h3>
-                
-                <table className="table table-hover table-borderless">
-                    <tbody className="coin_info">
-                        {displayCoinsInfo}
-                    </tbody>
-                </table>
-            </aside>
-
-            <aside className="losers">
-                <h3>Biggest Losers <span>24h</span> </h3>
-
-                <table className="table table-hover table-borderless">
-                    <tbody className="coin_info">
-                        {displayCoinsInfo}
-                    </tbody>
-                </table>
-            </aside>
-
-            <aside className="losers">
-                <h3>Trending</h3>
-
-                <table className="table table-hover table-borderless">
-                    <tbody className="coin_info">
-                        {displayCoinsInfo}
-                    </tbody>
-                </table>
-            </aside>
-        </div>
+        <tr className="coin_info_cont">
+            <th className="coin_text" scope="row">
+                <img alt="Coin Logo" src={props.image}></img>
+                <div className="crypto_text">
+                    <b className="coin_name">{props.name}</b>
+                    <p className="coin_name_short">{props.symbol}</p>
+                </div>
+            </th>
+            <td className="coin_info_num">
+                <p className="coin_price">${props.price}</p>
+                <p className="coin_change">{props.priceChange}</p>
+            </td>
+        </tr>
     )
 }
--- src/coins/Coins.js
+++ src/coins/Coins.js
@@ -1,59 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './Coins.css';
 
-export default function Coins() {
-    const [coins, setCoins] = useState([]);
-
-    useEffect(() => {
-        (async () => {
-            const endpoint = new URL("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false");
-
-            const response = await fetch(endpoint);
-            const data = await response.json();
-
-            setCoins(data);
-        })();
-    }, [])
-
-    const displayCoins = coins.map(coin => {
-        return (
-            <tr className="coin_cont" key={coin.id}>
-                <th className="coin_text" scope="row">
-                    <img alt="Coin Logo" src={coin.image}></img>
-                    <div className="crypto_text">
-                        <b className="coin_name">{coin.name}</b>
-                        <p className="coin_name_short">{coin.symbol}</p>
-                    </div>
-                </th>
-                <td className="coin_price">${coin.current_price}</td>
-                <td className="coin_change">{coin.price_change_percentage_24h}</td>
-                <td className="coin_mc">{coin.market_cap}</td>
-                <td className="coin_volume">${coin.total_volume}</td>
-                <td className="coin_supply">{coin.total_supply}</td>
-            </tr>
-        )
-    })
+export default function Coins(props) {
 
     return (
-        <main className="main">
-            <h3 className="main_title">Crypto prices <span>9999 assets</span> </h3>
-            
-            <table className="table table-hover table-borderless">
-                <thead className="parameters">
-                    <tr>
-                        <th className="par_name" scope="col">Name</th>
-                        <th className="par_price" scope="col">Price</th>
-                        <th className="par_change" scope="col">Change</th>
-                        <th className="par_mc" scope="col">Market cap</th>
-                        <th className="par_volume" scope="col">Volume 24h</th>
-                        <th className="par_supply" scope="col">Supply</th>
-                    </tr>
-                </thead>
-
-                <tbody className="coin_list">
-                    {displayCoins}
-                </tbody>
-            </table>
-        </main>
+        <tr className="coin_cont">
+            <th className="coin_text" scope="row">
+                <img alt="Coin Logo" src={props.image}></img>
+                <div className="crypto_text">
+                    <b className="coin_name">{props.name}</b>
+                    <p className="coin_name_short">{props.symbol}</p>
+                </div>
+            </th>
+            <td className="coin_price">${props.price}</td>
+            <td className="coin_change">{props.priceChange}</td>
+            <td className="coin_mc">{props.marketCap}</td>
+            <td className="coin_volume">${props.volume}</td>
+            <td className="coin_supply">{props.supply}</td>
+        </tr>
     )
 }
