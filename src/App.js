26706d8fc6f54471f03380978fbd2dac41917b81
AleexxG <alexanderg.sln@gmail.com>
2023-01-02 21:51:52 +0100

    Top Coins Display

--- src/App.js
+++ src/App.js
@@ -1,128 +1,16 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import Search from './search/Search';
 import Coins from './coins/Coins';
 import Info from  './Info/Info';
 
 function App() {
-
-  const [coins, setCoins] = useState([]);
-  const [coinsInfo, setCoinsInfo] = React.useState([]);
-
-  useEffect(() => {
-      (async () => {
-          const allCoins = new URL("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false");
-          const trendingCoins = new URL("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=5&page=1&sparkline=false&price_change_percentage=24h");
-
-          const response = await Promise.all([fetch(allCoins), fetch(trendingCoins)]);
-
-          const data1 = await response[0].json();
-          const data2 = await response[1].json();
-
-          setCoins(data1);
-          setCoinsInfo(data2);
-      })();
-  }, [])
-
-  const displayCoins = coins.map(coin => {
-    return (
-      <Coins
-        key={coin.id}
-        image={coin.image}
-        name={coin.name}
-        symbol={coin.symbol}
-        price={coin.current_price}
-        priceChange={coin.price_change_percentage_24h}
-        marketCap={coin.market_cap}
-        volume={coin.total_volume}
-        supply={coin.total_supply}
-      />
-    )
-  })
-
-  function topFiveGainers() {
-    coins.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h);
-    const topFive = coins.slice(0, 5);
-    console.log(topFive);
-  }
-
-  function topFiveLosers() {
-      coins.sort((a, b) => a.price_change_percentage_24h - b.price_change_percentage_24h);
-      const topFive = coins.slice(0, 5);
-      console.log(topFive);
-  }
-
-  const displayInfo = coinsInfo.map(coin => {
-    return (
-      <Info
-        key={coin.id}
-        image={coin.image}
-        name={coin.name}
-        symbol={coin.symbol}
-        price={coin.current_price}
-        priceChange={coin.price_change_percentage_24h}
-        gainers={topFiveGainers()}
-        losers={topFiveLosers()}
-      />
-    )
-  })
-
   return (
     <div className="App">
       <Search />
       <div className="content">
-        <main className='main'>
-          <h3 className="main_title">Crypto prices <span>9999 assets</span> </h3>
-          
-          <table className="table table-hover table-borderless">
-              <thead className="parameters">
-                  <tr>
-                      <th className="par_name" scope="col">Name</th>
-                      <th className="par_price" scope="col">Price</th>
-                      <th className="par_change" scope="col">Change</th>
-                      <th className="par_mc" scope="col">Market cap</th>
-                      <th className="par_volume" scope="col">Volume 24h</th>
-                      <th className="par_supply" scope="col">Supply</th>
-                  </tr>
-              </thead>
-
-              <tbody className="coin_list">
-                {displayCoins}
-              </tbody>
-          </table>
-        </main>
-
-        <div className='info'>
-            <aside className="gainers">
-                <h3>Biggest Gainers <span>24h</span> </h3>
-                
-                <table className="table table-hover table-borderless">
-                    <tbody className="coin_info">
-                      {displayInfo}
-                    </tbody>
-                </table>
-            </aside>
-
-            <aside className="losers">
-                <h3>Biggest Losers <span>24h</span> </h3>
-
-                <table className="table table-hover table-borderless">
-                    <tbody className="coin_info">
-                        {displayInfo}
-                    </tbody>
-                </table>
-            </aside>
-
-            <aside className="losers">
-                <h3>Trending</h3>
-
-                <table className="table table-hover table-borderless">
-                    <tbody className="coin_info">
-                      {displayInfo}
-                    </tbody>
-                </table>
-            </aside>
-        </div>
+        <Coins />
+        <Info />
       </div>
     </div>
   );
--- src/Info/Info.js
+++ src/Info/Info.js
@@ -1,21 +1,114 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Info.css';
 
-export default function Info(props) {
+export default function Info() {
+    const [coins, setCoins] = useState([]);
+    const [coinsInfo, setCoinsInfo] = React.useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const allCoins = new URL("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            const trendingCoins = new URL("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=5&page=1&sparkline=false&price_change_percentage=24h");
+
+            const response = await Promise.all([fetch(allCoins), fetch(trendingCoins)]);
+
+            const data1 = await response[0].json();
+            const data2 = await response[1].json();
+
+            setCoins(data1);
+            setCoinsInfo(data2);
+        })();
+    }, [])
+
+    const trendingCoins = coinsInfo.map(coin => {
+        return (
+            <tr className="coin_info_cont" key={coin.id}>
+                <th className="coin_text" scope="row">
+                    <img alt="Coin Logo" src={coin.image}></img>
+                    <div className="crypto_text">
+                        <b className="coin_name">{coin.name}</b>
+                        <p className="coin_name_short">{coin.symbol}</p>
+                    </div>
+                </th>
+                <td className="coin_info_num">
+                    <p className="coin_price">${coin.current_price}</p>
+                    <p className="coin_change">{coin.price_change_percentage_24h}</p>
+                </td>
+            </tr>
+        )
+    })
+
+    coins.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h);
+    const gainers = coins.slice(0, 5);
+    const topGainers = gainers.map(coin => {
+        return (
+            <tr className="coin_info_cont" key={coin.id}>
+                <th className="coin_text" scope="row">
+                    <img alt="Coin Logo" src={coin.image}></img>
+                    <div className="crypto_text">
+                        <b className="coin_name">{coin.name}</b>
+                        <p className="coin_name_short">{coin.symbol}</p>
+                    </div>
+                </th>
+                <td className="coin_info_num">
+                    <p className="coin_price">${coin.current_price}</p>
+                    <p className="coin_change">{coin.price_change_percentage_24h}</p>
+                </td>
+            </tr>
+        )
+    })
+
+    coins.sort((a, b) => a.price_change_percentage_24h - b.price_change_percentage_24h);
+    const losers = coins.slice(0, 5);
+    const topLosers = losers.map(coin => {
+        return (
+            <tr className="coin_info_cont" key={coin.id}>
+                <th className="coin_text" scope="row">
+                    <img alt="Coin Logo" src={coin.image}></img>
+                    <div className="crypto_text">
+                        <b className="coin_name">{coin.name}</b>
+                        <p className="coin_name_short">{coin.symbol}</p>
+                    </div>
+                </th>
+                <td className="coin_info_num">
+                    <p className="coin_price">${coin.current_price}</p>
+                    <p className="coin_change">{coin.price_change_percentage_24h}</p>
+                </td>
+            </tr>
+        )
+    })
 
     return(
-        <tr className="coin_info_cont">
-            <th className="coin_text" scope="row">
-                <img alt="Coin Logo" src={props.image}></img>
-                <div className="crypto_text">
-                    <b className="coin_name">{props.name}</b>
-                    <p className="coin_name_short">{props.symbol}</p>
-                </div>
-            </th>
-            <td className="coin_info_num">
-                <p className="coin_price">${props.price}</p>
-                <p className="coin_change">{props.priceChange}</p>
-            </td>
-        </tr>
+        <div className="info">
+            <aside className="gainers">
+                <h3>Biggest Gainers <span>24h</span> </h3>
+                
+                <table className="table table-hover table-borderless">
+                    <tbody className="coin_info">
+                        {topGainers}
+                    </tbody>
+                </table>
+            </aside>
+
+            <aside className="losers">
+                <h3>Biggest Losers <span>24h</span> </h3>
+
+                <table className="table table-hover table-borderless">
+                    <tbody className="coin_info">
+                        {topLosers}
+                    </tbody>
+                </table>
+            </aside>
+
+            <aside className="losers">
+                <h3>Trending</h3>
+
+                <table className="table table-hover table-borderless">
+                    <tbody className="coin_info">
+                        {trendingCoins}
+                    </tbody>
+                </table>
+            </aside>
+        </div>
     )
 }
