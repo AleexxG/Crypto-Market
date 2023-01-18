617525b29689a03a9ede0d2ea70697bde29bf879
AleexxG <alexanderg.sln@gmail.com>
2023-01-18 20:19:43 +0100

    Moving API

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Search from '../search/Search';
-import Coins from '../Coins';
+import CoinsTable from '../coinsTable/CoinsTable';
+import Info from '../Info/Info';
 
 function HomePage() {
+  const [coins, setCoins] = useState([]);
+    const [coinsInfo, setCoinsInfo] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const endpoint1 = new URL('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+            const endpoint2 = new URL('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=5&page=1&sparkline=false&price_change_percentage=24h');
+
+            const response = await Promise.all([fetch(endpoint1), fetch(endpoint2)]);
+
+            const data1 = await response[0].json();
+            const data2 = await response[1].json();
+
+            setCoins(data1);
+            setCoinsInfo(data2);
+        })();
+    }, [])
+
   return (
     <div className="App">
       <Search />
-      <Coins />
+      <main className="content">
+            <CoinsTable
+                coins={coins}
+            />
+            <Info
+                coins={coins}
+                coinsInfo={coinsInfo}
+            />
+        </main>
     </div>
   )
 }
