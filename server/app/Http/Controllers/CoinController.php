5435da64e4711421d14e03e85d7477c06d2873a6
AleexxG <alexanderg.sln@gmail.com>
2024-03-05 12:49:45 +0100

    Coin list api route done

--- server/app/Http/Controllers/CoinController.php
+++ server/app/Http/Controllers/CoinController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\GetCoinListRequest;
+use App\Models\Coin;
+use App\Models\FiatCurrency;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CoinController extends Controller
 {
-    public function coinList(GetCoinListRequest $request)
+    public function coinList(GetCoinListRequest $request): JsonResponse
     {
-        //
+        $currencyCode = strtoupper($request->get('currency'));
+        $currency = FiatCurrency::where('code', $currencyCode)->first();
+
+        $offset = ($request->get('page') - 1) * Coin::COINS_PER_PAGE;
+
+        $coins = Coin::with(['coinFiatMetrics' => function ($query) use ($currency) {
+            $query->where('fiat_currency_id', $currency->id);
+        }])
+        ->skip($offset)
+        ->take(Coin::COINS_PER_PAGE)
+        ->get();
+
+        return response()->json($coins);
     }
 }
