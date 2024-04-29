756fe811af1e9745798bca09aeeaa06aff798f4b
AleexxG <alexanderg.sln@gmail.com>
2024-04-29 19:00:59 +0200

    Remove market data component on small screens

--- client/src/components/home/Market.jsx
+++ client/src/components/home/Market.jsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react';
 import MarketData from './market/MarketData';
 import TopGainers from './market/TopGainers';
 import FollowUs from './market/TopLosers';
 
 function Market() {
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('resize', () => setIsSmallScreen(window.innerWidth > 992));
+        return () => {
+            window.removeEventListener('resize', () => setIsSmallScreen(window.innerWidth > 992));
+        };
+    }, []);
 
     return (
         <section className='gradient pt-md-4 pt-3 pb-5'>
             <div className='container py-4 d-flex flex-md-row flex-column justify-content-between gap-3'>
                 
-                <MarketData />
+                {isSmallScreen && <MarketData />}
 
                 <TopGainers />
                 
